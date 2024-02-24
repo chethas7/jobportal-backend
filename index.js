fd94a03b25d50cc6b7ad05751d90c50913002b44
chethas <chethas7@gmail.com>
2024-02-24 15:01:47 +0530

    Initial commit with untracked files

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+require("dotenv").config();
+console.log(process.env.MONGODB);
+
+//Middlewares
+app.use(express.json());
+app.use(cors());
+
+//Mongo Connection
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGODB;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    //CREATE DATABASE
+    const database = client.db("JobPortal");
+    const jobsCollections = database.collection("availableJobs");
+    //get all jobs
+    app.get("/all-jobs", async (req, res) => {
+      const jobs = await jobsCollections.find({}).toArray();
+      res.send(jobs);
+    });
+
+    //get jobs by email
+    app.get("/myJobs/:email", async (req, res) => {
+      const jobs = await jobsCollections
+        .find({ postedBy: req.params.email })
+        .toArray();
+      res.send(jobs);
+    });
+
+    //post Job
+    app.post("/post-job", async (req, res) => {
+      const body = req.body;
+      body.createAt = new Date();
+      console.log(body);
+      const result = await jobsCollections.insertOne(body);
+      if (result.insertedId) {
+        return res.status(200).send(result);
+      } else {
+        return res.status(404).send({
+          message: "cannot insert",
+          status: false,
+        });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Developer");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
